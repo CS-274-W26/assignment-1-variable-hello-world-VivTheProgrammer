028383abb2917034f07b9d5582e0b5d134b9d84a
Vivaan Tinwalla <vivaantinwalla.005@gmail.com>
2026-01-19 19:06:19 -0800

    i am done with the work

--- prog1.c
+++ prog1.c
@@ -1,11 +1,24 @@
-#include <stdio.h> // Necessary for printf()
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-// TODO Other includes
+int main(){
+	int random_number;
 
+	srand(time(NULL)); // seeds the PRNG thingy
 
-int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
+	random_number = rand() % 10; // starts generating a random number between 0 and 10
+
+	if (random_number >0 && random_number <=4){
+		printf("Eat more beef, kick less cats\n");
 	
-	// TODO Complete the program
+	} else if (random_number >= 5 && random_number <=9){
+		printf("FRODO LIVES\n");
+	
+	} else {
+		printf("Larn is the best rougelike\n");
+
+	}
+
+	printf("The random number was: %d\n", random_number);
 }
